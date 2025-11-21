fc918cdd5f0349e84612f16415ad116b7702c08d
dachishengelia <shengeliaqeti111@gmail.com>
2025-11-21 19:59:28 +0400

    test

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE,
-    withCredentials: true, // Send cookies with requests
+    baseURL: import.meta.env.VITE_API_BASE, // Backend API base URL
+    withCredentials: true, // Include cookies in requests
 });
 
 axiosInstance.interceptors.response.use(
