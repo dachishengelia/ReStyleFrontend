02309935715f3a0dd19a268525a5bf2b0b924aa5
dachishengelia <shengeliaqeti111@gmail.com>
2025-12-20 14:54:46 +0400

    cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -9,7 +9,7 @@ export default function CartPage() {
     if (quantity < 1) quantity = 1;
     updateCart(productId, quantity);
   };
-asdadadasdasd
+
   return (
     <div className="p-6 max-w-4xl mx-auto bg-white rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-6">Your Cart</h2>
